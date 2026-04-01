0563391f4507542fc6e2650d4c0cdc0328bf860a
Bruno Guimarães <xxbrunoguimaraes@gmail.com>
2026-04-01 15:42:13 -0300

    fonts

--- app/(tabs)/subscriptions.tsx
+++ app/(tabs)/subscriptions.tsx
@@ -4,11 +4,13 @@ import {styled} from "nativewind";
 import {SafeAreaView as RNSafeAreaView} from "react-native-safe-area-context";
 const SafeAreaView =  styled(RNSafeAreaView);
 
-const Subscriptions = () => {
+const Subscriptions  = () => {
     return (
        <SafeAreaView className = " flex-1 bg-background p-5">
            <Text>Subscriptions</Text>
        </SafeAreaView>
     )
 }
-export default Subscriptions;
+
+
+export default Subscriptions
